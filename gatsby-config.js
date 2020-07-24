44fb006595c397f927ea05dc53e49aadd6f85159
jbuyu <jbuyu@ymail.com>
2020-07-24 12:36:10 +0300

    fixes for project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `DoqHq Kenya`,
+    description: `Call and receive help from accredited medical practitioners in kenya.`,
+    images: "/src/images/gatsby-icon.png",
+    twitterUsername: "@buyujustus",
+    author: "Justus Buyu",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +29,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
