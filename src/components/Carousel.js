7939d58b66d3898ce234727ca20c8cf8d70733a7
jbuyu <jbuyu@ymail.com>
2020-08-01 16:45:21 +0300

    misc fixes with carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,28 +1,5 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 
-import BackgroundSlider from "gatsby-image-background-slider"
-import { useStaticQuery } from "graphql"
-
-const Carousel = () => (
-  <>
-    <BackgroundSlider
-      query={useStaticQuery(graphql`
-        query {
-          backgrounds: allFile(
-            filter: { sourceInstanceName: { eq: "backgrounds" } }
-          ) {
-            nodes {
-              relativePath
-              childImageSharp {
-                fluid(maxWidth: 4000, quality: 100) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
-      `)}
-    />
-  </>
-)
+const Carousel = () => <div></div>
 export default Carousel
