2c5281c653a8b08054a7bf96f95daf0cac558b67
jbuyu <jbuyu@ymail.com>
2020-08-08 20:02:26 +0300

    adds services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,16 +1,38 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faUserMd } from "@fortawesome/free-solid-svg-icons"
+import {
+  faUserMd,
+  faBabyCarriage,
+  faVials,
+  faUserNurse,
+  faIdCard,
+} from "@fortawesome/free-solid-svg-icons"
 const Services = () => {
   return (
     <div className="services">
       <h4 className="services-header">
         <span>our</span> Services
       </h4>
-      <div className="service-types">
-        <h5>
-          <FontAwesomeIcon icon={faUserMd} /> General Medical Consultation
-        </h5>
+      <div>
+        <ul className="service-types">
+          <li>
+            <FontAwesomeIcon icon={faUserMd} /> General Medical Consultation
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBabyCarriage} /> Home Based Antenatal
+            Clinic
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faVials} /> Laboratory diagnosis
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faUserNurse} /> Home Based Nursing care
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faIdCard} /> Admission and Follow within
+            kisumu
+          </li>
+        </ul>
       </div>
     </div>
   )
