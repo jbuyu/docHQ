be3ad8fd19b96728c6d9f0b92b647fffc538aabb
jbuyu <jbuyu@ymail.com>
2020-08-11 10:13:22 +0300

    adds contentful lplugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,13 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `your_space_id_grab_it_from_contentful`,
+        accessToken: `your_token_id_grab_it_from_contentful`,
+      },
+    },
     // {
     //   resolve: `gatsby-source-filesystem`,
     //   options: {
