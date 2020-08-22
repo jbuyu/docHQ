f694a361d37091d30229aca291ba70e3c2bfcad3
jbuyu <jbuyu@ymail.com>
2020-08-22 14:13:59 +0300

    changes locatopn for url

--- src/components/seo.js
+++ src/components/seo.js
@@ -55,7 +55,7 @@ const Seo = ({ description, keywords, title, image, url, author }) => {
               },
               {
                 property: `og:url`,
-                content: "https://kisumuhomedoctors.com/",
+                content: "http://kisumuhomedoctors.com/",
               },
               {
                 name: `twitter:card`,
