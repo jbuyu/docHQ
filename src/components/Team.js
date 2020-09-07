865ba4339220a77eeadd6e7142f7bf278860b554
jbuyu <jbuyu@ymail.com>
2020-09-07 21:04:02 +0300

    changes image loading from cms

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,58 +1,92 @@
 import React from "react"
-// import { useStaticQuery } from "gatsby"
-// import Img from "gatsby-image"
-const Team = ({ doctors }) => {
-  // const data = useStaticQuery(graphql`
-  //   query Images {
-  //     moroga: file(relativePath: { eq: "moroga.jpg" }) {
-  //       childImageSharp {
-  //         fluid {
-  //           ...GatsbyImageSharpFluid
-  //         }
-  //       }
-  //     }
-  //     ominde: file(relativePath: { eq: "ominde.jpg" }) {
-  //       childImageSharp {
-  //         fluid {
-  //           ...GatsbyImageSharpFluid
-  //         }
-  //       }
-  //     }
-  //     faith: file(relativePath: { eq: "faith.jpg" }) {
-  //       childImageSharp {
-  //         fluid {
-  //           ...GatsbyImageSharpFluid
-  //         }
-  //       }
-  //     }
-  //     oyoo: file(relativePath: { eq: "oyoo.jpg" }) {
-  //       childImageSharp {
-  //         fluid {
-  //           ...GatsbyImageSharpFluid
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+import { useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+const Team = () => {
+  const data = useStaticQuery(graphql`
+    query Images {
+      doc1: file(relativePath: { eq: "oyoo.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      doc2: file(relativePath: { eq: "moroga.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      doc3: file(relativePath: { eq: "faith.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      doc4: file(relativePath: { eq: "ominde.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      doc5: file(relativePath: { eq: "akuom.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
     <div id="team" className="meet-team">
       <h1 className="heading">
         <span>meet</span> Our Team
       </h1>
       <ul className="profiles">
-        {doctors.map((doc, index) => {
-          return (
-            <li className="profile" key={index}>
-              <img
-                className="profile-img"
-                src={doc.image.asset.url}
-                alt="doctor_image"
-              />
-              <h4 className="user-name">{doc.name}</h4>
-              <h5>{doc.bio}</h5>
-            </li>
-          )
-        })}
+        <li className="profile">
+          <Img
+            className="profile-img"
+            fluid={data.doc1.childImageSharp.fluid}
+          />
+          <h4 className="user-name">Dr. Oyoo Carl(MBChB)</h4>
+          <h5>General Practitioner</h5>
+        </li>
+        <li className="profile">
+          <Img
+            className="profile-img"
+            fluid={data.doc2.childImageSharp.fluid}
+          />
+          <h4 className="user-name">Dr. William Moroga(MBChB)</h4>
+          <h5>General Practitioner</h5>
+        </li>
+        <li className="profile">
+          <Img
+            className="profile-img"
+            fluid={data.doc3.childImageSharp.fluid}
+          />
+          <h4 className="user-name">Dr. Faith Ondago (MBChB)</h4>
+          <h5>General Practitioner</h5>
+        </li>
+        <li className="profile">
+          <Img
+            className="profile-img"
+            fluid={data.doc4.childImageSharp.fluid}
+          />
+          <h4 className="user-name">Dr. Ominde Francis (MBChB)</h4>
+          <h5>General Practitioner</h5>
+        </li>
+        <li className="profile">
+          <Img
+            className="profile-img"
+            fluid={data.doc5.childImageSharp.fluid}
+          />
+          <h4 className="user-name">Dr. David Akuom (MBChB)</h4>
+          <h5>General Practitioner</h5>
+        </li>
       </ul>
     </div>
   )
